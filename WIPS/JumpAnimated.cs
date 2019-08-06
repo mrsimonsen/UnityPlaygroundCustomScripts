b77ea6e9ebc3414e2efce1c1bc91d3480f4671c6
Mr. Simonsen <johnsimonsen@weber.edu>
2019-08-06 11:16:43 -0600

    Create JumpAnimated.cs
    started

---
+++ WIPS/JumpAnimated.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Playground/Movement/Jump With Animation")]
+[RequireComponent(typeof(Rigidbody2D))]
+public class JumpAnimated : Physics2DObject
+{
+	[Header("Jump setup")]
+	// the key used to activate the push
+	public KeyCode key = KeyCode.Space;
+
+	// strength of the push
+	public float jumpStrength = 10f;
+
+	[Header("Ground setup")]
+	//if the object collides with another object tagged as this, it can jump again
+	public string groundTag = "Ground";
+
+	//this determines if the script has to check for when the player touches the ground to enable him to jump again
+	//if not, the player can jump even while in the air
+	public bool checkGround = true;
+
+	private bool canJump = true;
+
+	[Header("Animation")]
+	public Animator animator;
+	public string var = "name of animation parameter";
+
+	// Read the input from the player
+	void Update()
+	{
+		if(canJump
+			&& Input.GetKeyDown(key))
+		{
+			// Apply an instantaneous upwards force
+			rigidbody2D.AddForce(Vector2.up * jumpStrength, ForceMode2D.Impulse);
+			canJump = !checkGround;
+		}
+	}
+
+	private void OnCollisionEnter2D(Collision2D collisionData)
+	{
+		if(checkGround
+			&& collisionData.gameObject.CompareTag(groundTag))
+		{
+			canJump = true;
+		}
+	}
+}
