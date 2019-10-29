cb8ab5e2b406cdd5750eb5bdfd5e72248ba24e6e
John Simonsen <johnsimonsen@weber.edu>
2019-10-29 14:40:06 -0600

    Update DayNight.cs

--- WIPS/DayNight.cs
+++ WIPS/DayNight.cs
@@ -1,22 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-//https://answers.unity.com/questions/983470/unity-2d-daynight.html
-
-[AddComponentMenu("Playground/Day & Night Timer")]
-public class DayNight : MonoBehaviour{
-  private int currentTime = 0f; //game time starts at 0
-  private bool isDay; //is it day or night
+using UnityEngine.UI;
+//https://docs.unity3d.com/2018.4/Documentation/ScriptReference/UI.Image.html
+public class DayNight : MonoBehaviour
+{
+  private float currentTime;
+  private bool isDay;
+  private bool transition = false;
+  public int dayDuration = 10;//how long day lasts in seconds
+  public int nightDuration = 10;//how long night lasts in seconds
   public bool startDay = true; //start the game in day or night?
-  public int dayLength = 10; //length of the day
-  public int nightLength = 10; //length of the night
-  public int transitionSpeed = 2; //length of dawn and dusk
-  public float maxDark = 0.5f;
+  public float transitionSpeed = 2f; //length of dawn and dusk transition in seconds
+  [Range(0f,1f)]
+  public float maxDark = 0.5f;//max percentage of the alpha channel
+  public Image image;
+  private Color color;
 
-  /*
-  create a top layer that is transparent black
-  0 opacity during the day
-  max opacity during the night
-  positive transition for dawn
-  negative transition for dusk
-  */
+  void Start(){
+    currentTime = 0f;
+    if (startDay){
+      isDay = true;
+      color = new Color (0f,0f,0f,0f);//overlay is clear
+      image.color = color;
+    }
+    else{
+      isDay = false;
+      color = new Color (0f,0f,0f,maxDark);//overlay is dark
+      image.color = color;
+    }
+    Debug.Log(image.color);
+  }
 
+  void Update(){
+    currentTime += Time.deltaTime;
+    if (isDay && currentTime>dayDuration && !transition){//trigger dusk
+      isDay = false;
+      transition = true;
+    }
+    else if (!isDay && currentTime>nightDuration && !transition){//trigger dawn
+      isDay = true;
+      transition = true;
+    }
+    else if (transition){
+      transition = dawnDusk(transition);
+      if (!transition){
+        currentTime = Time.deltaTime;//reset timer when transition is complete
+      }
+    }
+    Debug.Log(currentTime);
+    Debug.Log(image.color);
+
+  }
+  private bool dawnDusk(bool running){
+    int dark = (255 * maxDark) * transitionSpeed;
+    if (isDay){//dawn
+      image.color.a -= dark;
+      if (image.color.a == 0){
+        running = false;
+      }
+    }
+    else{//dusk
+      image.color.a += dark;
+      if (image.color.a <= (255*maxDark)){
+        running = false;
+      }
+    }
+    return running;
+  }
 }
